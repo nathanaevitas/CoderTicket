a143e2d352da2b2a9c34f2330237f4efb19ebe26
Nathan Tran <nathan@aevitasgroup.com>
2015-12-25 21:05:17 +0700

    for debugging heroku app

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'calculates the subtotal and save to database' do
+  it 'calculates the correct total cost of an order' do
   	order = Order.create
   	order_item = OrderItem.new(price: 2, quantity: 10)
   	order_item.order = order
