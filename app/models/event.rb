f3313f4c05738068fd7ca211be560a1a1874b43b
Nathan Tran <nathan@aevitasgroup.com>
2015-12-26 22:50:08 +0700

    Change Event class methods to scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,16 @@ class Event < ActiveRecord::Base
   validates_presence_of :extended_html_description, :venue, :category, :starts_at
   validates_uniqueness_of :name, uniqueness: {scope: [:venue, :starts_at]}
 
-  def self.current_events
-  	Event.where('starts_at >= ?',Time.new)  	
-  end
+  # def self.current_events
+  # 	Event.where('starts_at >= ?',Time.new)  	
+  # end
+  scope :current_events, -> {where('starts_at > ?',Time.new)}
+  # def self.search(str)
+  # 	param = str+'%'
+  # 	Event.where('name LIKE ? or extended_html_description LIKE ?',param,param)
+  # end
 
-  def self.search(str)
-  	param = str+'%'
-  	Event.where('name LIKE ? or extended_html_description LIKE ?',param,param)
-  end
+  scope :search, -> (str) {where('name LIKE ? or extended_html_description LIKE ?',str+'%',str+"%")}
 
   def self.event_list
       Event.all
